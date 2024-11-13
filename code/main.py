3bc51692e86629747ed3139af9f8b63a8f5f6f2a
Khvyts-Taras <Khvyts2005@gmail.com>
2024-11-13 23:51:34 +0200

    ну не помню

--- code/main.py
+++ code/main.py
@@ -9,212 +9,170 @@ import torch.nn.functional as F
 from torch import autograd
 import math
 
-# Arguments
+
 BATCH_SIZE = 128
 IMGS_TO_DISPLAY = 100
-EPOCHS = 500
+EPOCHS = 50
 Z_DIM = 100
-CHANNELS = 3
 N_CRITIC = 5
-GRADIENT_PENALTY = 10
+GRADIENT_PENALTY = 100
 LOAD_MODEL = False
 
 
-model_path = 'model'
+model_path = 'models'
+samples_path = 'samples'
+os.makedirs(model_path, exist_ok=True)
+os.makedirs(samples_path, exist_ok=True)
 
-os.makedirs('model', exist_ok=True)
-os.makedirs('samples', exist_ok=True)
-
-# Method for storing generated images
-def generate_imgs(z, epoch=0):
-	generator.eval()
-	fake_imgs = generator(z)
-	fake_imgs_ = vutils.make_grid(fake_imgs, normalize=True, nrow=math.ceil(z.shape[0] ** 0.5))
-	vutils.save_image(fake_imgs_, os.path.join('samples', 'sample_' + str(epoch) + '.png'))
-
-
-# Data loaders
-transform = transforms.Compose([transforms.Resize([64, 64]),
-								transforms.ToTensor(),
-								transforms.Normalize([0.5], [0.5])])
-
-
-# Конфигурация загрузчика данных
-os.makedirs("/data/Places365", exist_ok=True)
-dataset = datasets.Places365("/data/Places365", split='val', small=True, download=False, transform=transform)
-
-data_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True,
-										  drop_last=True)
-
-# Fix images for viz
-fixed_z = torch.randn(IMGS_TO_DISPLAY, Z_DIM)
-
-# Labels
-real_label = torch.ones(BATCH_SIZE)
-fake_label = torch.zeros(BATCH_SIZE)
-
-
-class Generator(nn.Module):
-    def __init__(self, z_dim=100, channels=3, conv_dim=64):
-        super(Generator, self).__init__()
-        self.net = nn.Sequential(
-            nn.ConvTranspose2d(z_dim, conv_dim * 8, 4, 2, 0, bias=False),
-            nn.BatchNorm2d(conv_dim * 8),
-            nn.ReLU(),
-            nn.ConvTranspose2d(conv_dim * 8, conv_dim * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(conv_dim * 4),
-            nn.ReLU(),
-            nn.ConvTranspose2d(conv_dim * 4, conv_dim * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(conv_dim * 2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(conv_dim * 2, conv_dim, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(conv_dim),
-            nn.ReLU(),
-            nn.ConvTranspose2d(conv_dim, channels, 4, 2, 1, bias=True),
-            nn.Tanh()
-        )
-        self._initialize_weights()
-
-    def forward(self, x):
-        x = x.reshape([x.shape[0], -1, 1, 1])
-        return self.net(x)
-    
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.ConvTranspose2d):
-                nn.init.normal_(m.weight, 0.0, 0.02)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.normal_(m.weight.data, 1.0, 0.02)
-                nn.init.constant_(m.bias.data, 0)
-
-class Critic(nn.Module):
-    def __init__(self, channels=3, conv_dim=64):
-        super(Critic, self).__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(channels, conv_dim, 4, 2, 1, bias=True),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(conv_dim, conv_dim * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(conv_dim * 2),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(conv_dim * 2, conv_dim * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(conv_dim * 4),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(conv_dim * 4, conv_dim * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(conv_dim * 8),
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(conv_dim * 8, 1, 4, 1, 0, bias=True)
-        )
-        self._initialize_weights()
-
-    def forward(self, x):
-        return self.net(x).squeeze()
-    
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.normal_(m.weight, 0.0, 0.02)
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.normal_(m.weight.data, 1.0, 0.02)
-                nn.init.constant_(m.bias.data, 0)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def gradient_penalty(real, fake):
-	m = real.shape[0]
-	epsilon = torch.rand(m, 1, 1, 1)
-	if is_cuda:
-		epsilon = epsilon.cuda()
+	epsilon = torch.rand(real.shape[0], 1, 1, 1).to(device)
 	
 	interpolated_img = epsilon * real + (1-epsilon) * fake
 	interpolated_out = critic(interpolated_img)
 
 	grads = autograd.grad(outputs=interpolated_out, inputs=interpolated_img,
-							   grad_outputs=torch.ones(interpolated_out.shape).cuda() if is_cuda else torch.ones(interpolated_out.shape),
-							   create_graph=True, retain_graph=True)[0]
-	grads = grads.reshape([m, -1])
+						  grad_outputs=torch.ones(interpolated_out.shape).to(device),
+						  create_graph=True, retain_graph=True)[0]
+
+	grads = grads.reshape([real.shape[0], -1])
 	grad_penalty = ((grads.norm(2, dim=1) - 1) ** 2).mean()
 	return grad_penalty
 
 
-generator = Generator(z_dim=Z_DIM, channels=CHANNELS)
-critic = Critic(channels=CHANNELS)
+def generate_imgs(z, epoch=0):
+	generator.eval()
+	fake_imgs = generator(z)
+	fake_imgs_ = vutils.make_grid(fake_imgs, normalize=True, nrow=math.ceil(z.shape[0] ** 0.5))
+	vutils.save_image(fake_imgs_, os.path.join(samples_path, 'sample_' + str(epoch) + '.png'))
 
-# Load previous model   
+
+class Gen(nn.Module):
+    def __init__(self, z_dim=100, conv_dim=32):
+        super(Gen, self).__init__()
+        self.net = nn.Sequential(
+            nn.ConvTranspose2d(z_dim, conv_dim*8, 4, 2, 0, bias=False),
+            nn.InstanceNorm2d(conv_dim * 8),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(conv_dim*8, conv_dim*4, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(conv_dim*4),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(conv_dim*4, conv_dim*2, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(conv_dim*2),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(conv_dim*2, conv_dim, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(conv_dim),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(conv_dim, 3, 4, 2, 1, bias=True),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = x.reshape([x.shape[0], -1, 1, 1])
+        return self.net(x)
+
+
+class Critic(nn.Module):
+    def __init__(self, conv_dim=32):
+        super(Critic, self).__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(3, conv_dim, 4, 2, 1, bias=True),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(conv_dim, conv_dim*2, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(conv_dim*2),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(conv_dim*2, conv_dim*4, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(conv_dim * 4),
+            nn.LeakyReLU(0.2),
+
+            nn.Conv2d(conv_dim*4, conv_dim*8, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(conv_dim*8),
+            nn.LeakyReLU(0.2),
+            
+            nn.Conv2d(conv_dim*8, 1, 4, 1, 0, bias=True)
+        )
+
+    def forward(self, x):
+        return self.net(x).squeeze()
+
+
+
+
+transform = transforms.Compose([transforms.Resize([64, 64]),
+								transforms.ToTensor(),
+								transforms.Normalize([0.5], [0.5])])
+dataset = datasets.CelebA(root='/data/CelebA', split='train', transform=transform, download=True)
+data_loader = torch.utils.data.DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=False)
+
+
+generator = Gen(Z_DIM).to(device)
+critic = Critic().to(device)
+ 
 if LOAD_MODEL:
 	generator.load_state_dict(torch.load(os.path.join(model_path, 'generator.pkl')))
 	critic.load_state_dict(torch.load(os.path.join(model_path, 'critic.pkl')))
 
-
-# Define Optimizers
 g_opt = optim.Adam(generator.parameters(), lr=0.001, betas=(0.0, 0.9), weight_decay=2e-5)
 c_opt = optim.Adam(critic.parameters(), lr=0.001, betas=(0.0, 0.9), weight_decay=2e-5)
 
-# GPU Compatibility
-is_cuda = torch.cuda.is_available()
-if is_cuda:
-	generator, critic = generator.cuda(), critic.cuda()
-	real_label, fake_label = real_label.cuda(), fake_label.cuda()
-	fixed_z = fixed_z.cuda()
 
 total_iters = 0
-g_loss = d_loss = torch.Tensor([0])
+g_loss = c_loss = torch.Tensor([0])
 max_iter = len(data_loader)
-
-# Training
+fixed_z = torch.randn(IMGS_TO_DISPLAY, Z_DIM).to(device)
 for epoch in range(EPOCHS):
 	generator.train()
 	critic.train()
 
 	for i, data in enumerate(data_loader):
-
 		total_iters += 1
 
 		# Loading data
-		x_real, _ = data
-		z_fake = torch.randn(BATCH_SIZE, Z_DIM)
-
-		if is_cuda:
-			x_real = x_real.cuda()
-			z_fake = z_fake.cuda()
-
-		# Generate fake data
-		x_fake = generator(z_fake)
+		x_real = data[0].to(device)
+		z = torch.randn(x_real.shape[0], Z_DIM).to(device)
+		x_fake = generator(z)
 
 		# Train Critic
 		fake_out = critic(x_fake.detach())
 		real_out = critic(x_real.detach())
 		x_out = torch.cat((real_out, fake_out))
-		d_loss = (real_out.mean() - fake_out.mean()) + gradient_penalty(x_real, x_fake) * GRADIENT_PENALTY + (x_out ** 2).mean() * 0.0001
+		c_loss = (real_out.mean() - fake_out.mean()) + gradient_penalty(x_real, x_fake)*GRADIENT_PENALTY + (x_out ** 2).mean()*0.0001
 
 		c_opt.zero_grad()
-		d_loss.backward()
+		c_loss.backward()
 		c_opt.step()
 
 		# Train Generator
 		if total_iters % N_CRITIC == 0:
-			z_fake = torch.randn(BATCH_SIZE, Z_DIM)
-			if is_cuda:
-				z_fake = z_fake.cuda()
-			x_fake = generator(z_fake)
+			z = torch.randn(BATCH_SIZE, Z_DIM).to(device)
+			x_fake = generator(z)
 
 			fake_out = critic(x_fake)
-			g_loss =  fake_out.mean()
+			g_loss = fake_out.mean()
 
 			g_opt.zero_grad()
 			g_loss.backward()
 			g_opt.step()
 
+
 		if i % 50 == 0:
 			print("Epoch: " + str(epoch + 1) + "/" + str(EPOCHS)
 				  + "\titer: " + str(i) + "/" + str(max_iter)
 				  + "\ttotal_iters: " + str(total_iters)
-				  + "\td_loss:" + str(round(d_loss.item(), 4))
+				  + "\td_loss:" + str(round(c_loss.item(), 4))
 				  + "\tg_loss:" + str(round(g_loss.item(), 4))
 				  )
 			
+			generate_imgs(fixed_z, epoch=epoch + 1)
 
 	torch.save(generator.state_dict(), os.path.join(model_path, 'generator.pkl'))
 	torch.save(critic.state_dict(), os.path.join(model_path, 'critic.pkl'))
-	generate_imgs(fixed_z, epoch=epoch + 1)
-	
-
 generate_imgs(fixed_z)
